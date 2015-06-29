e924bd4fce755ae1218665b24d54415b16617797
Dravid Joseph <dravidjoseph@gmail.com>
2015-06-28 22:33:14 -0500

    updated isEmpty and size methods

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -14,18 +14,32 @@ LinkedList::~LinkedList(){
 }
 
 bool LinkedList::isEmpty() const{
-	
+	return m_size == 0;
 }
 
 int LinkedList::size() const{
-	
+	return m_size;
 }
 
 void LinkedList::printList() const{
 	
 }
 
-void LinkedList addBack(int value){
+void LinkedList::addBack(int value){
 	
 }
+
+void LinkedList::addFront(int value){
+	
+}
+
+bool LinkedList::removeBack(){
+	
+}
+
+bool LinkedList::removeFront(){
+	
+}
+
+
 
