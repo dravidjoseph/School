d76fb9b67aa9e18850fba00746fe0e9c99c5ef78
Dravid Joseph <dravidjoseph@gmail.com>
2015-07-19 22:47:30 -0500

    needs documentation

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -6,73 +6,116 @@
 ********************************************************/
 
 #include <iostream>
-#include "AnimalPen.h"
-#include "FarmAnimal.h"
 #include "Stack.h"
+#include "Node.h"
+#include "PreconditionViolationException.h"
+#include "AnimalPen.h"
+#include "Cow.h"
+#include "Chicken.h"
+#include "CyberChicken.h"
 
-/**
-void goodbyeMessage(const FarmAnimal& animal){
-	
-	std::cout<<"Upon release the"<<animal.getName()<<" said "<<animal.getSound()<<".\n";
-	
-}
-*/
 
-void printMenu(){
-	
-	std::cout<<" Select an animal to add to the pen: \n";
-	std::cout<<"1.) Cow (produces milk)\n";
-	std::cout<<"Chicken (cannot lay eggs)\n";
-	std::cout<<"CyberChicken (seems dangerous, but lays eggs)\n";
-	std::cout<<"---------------------------------------------------\n";
+
+void goodByeMessage(const FarmAnimal& animal){
+      std::cout<<"Upon release the "<<animal.getName()<<" said "<<animal.getSound()<<".\n";
 }
+
 
 int main(){
-	
-	printMenu();
-/**	
-	int choice = 0;
-	bool flag = false;
-	
-	std::string done = "";
-	
-	double value = 0.0;
-	int eggValue = 0;
-	
-	double totGallons = 0.0;
-	int totEggs = 0;
 
-	while(!flag){
-	
-		printMenu();
-		std::cout<<"Choice: ";
-		std::cin>>choice;
-	
-		if(choice == 1){
-			std::cout<<"\nHow many gallons of milk did this cow produce?";
-			std::cin>>value;
-			totGallons += value;
-		}
-		else if(choice == 2){
-			std::cout<<"\nAdd an eggless chicken to the pen? OK.  You're the boss." ;
-			std::cin>>value;
-		}
-		else if (choice == 3){
-			std::cout<<"How many eggs did this cyberchicken produce?";
-		}
-		else{
-			std::cout<<"Invalid choice";
-		}
-		
-		std::cout<<"Done adding animals? (y/n)";
-		std::cin>>done;
-		
-		if(done == "y"){
-			flag = true;
-		}
-	
-	}	
-*/
-	
-	return 0;	
+      AnimalPen pen;
+
+
+      bool flag = false;
+      int choice = 0;
+      double value = 0.0;
+      double totalMilk = 0.0;
+      double totalEggs = 0.0;
+      std::string done = "";
+      Cow* cow = nullptr;
+      Chicken* chicken = nullptr;
+      CyberChicken* cyber = nullptr;
+      FarmAnimal *animal = nullptr;
+
+      while(!flag){
+            std::cout<<"Select an animal to add to the pen: \n";
+            std::cout<<"1.) Cow (produces milk)\n";
+            std::cout<<"2.) Chicken (cannot lay eggs)\n";
+            std::cout<<"3.) Cyber Chicken (seems dangerous, but lays eggs)";
+            std::cout<<"\n---------------------------------------------------\n";
+
+            std::cout<<"choice: ";
+            std::cin>>choice;
+
+            if(choice == 1){
+                  cow = new Cow();
+
+                  std::cout<<"How many gallons of milk did this cow produce?: ";
+                  std::cin>>value;
+                  cow->setMilkProduced(value);
+                  pen.addAnimal(cow);
+                  totalMilk = totalMilk + value;
+
+            }
+            else if(choice == 2){
+                  chicken = new Chicken();
+
+                  std::cout<<"Add eggless chicken to the pen? OK. You're the boss.\n ";
+                  pen.addAnimal(chicken);
+
+            }
+            else if(choice == 3){
+                  cyber = new CyberChicken();
+
+                  std::cout<<"How many eggs did this cyber chicken produce?: ";
+                  std::cin>>value;
+                  cyber->setCyberEggs(value);
+                  pen.addAnimal(cyber);
+                  totalEggs = totalEggs + value;
+
+            }
+            std::cout<<"Done adding animals? (y/n):";
+            std::cin>>done;
+            if(done == "y"){
+                  flag = true;
+            }
+
+
+
+      }
+
+      std::cout<<"Releasing all animals!\n";
+      std::cout<<"-------------------------\n";
+      while(!pen.isPenEmpty()){
+
+            animal = pen.peekAtNextAnimal();
+            done = animal->getName();
+
+
+            if(done == "Cow"){
+                  cow = static_cast<Cow*>(animal);
+                  std::cout<<"This "<<done<<" produced "<<cow->getMilkProduced()<<" gallons of milk\n";
+
+            }
+            else if(done == "Chicken"){
+                  chicken = static_cast<Chicken*>(animal);
+                  std::cout<<"Chicken unable to lay eggs.  Perhaps Cybornetic implants will help?\n";
+            }
+            else{
+                  cyber = static_cast<CyberChicken*>(animal);
+                  std::cout<<"This "<<done<<" produced "<<cyber->getCyberEggs()<<" eggs\n";
+            }
+
+            goodByeMessage(*pen.peekAtNextAnimal());
+            std::cout<<done<< " destructor called.\n";
+            pen.releaseAnimal();
+      }
+
+
+      std::cout<<"Your farm produced "<<totalMilk<<" gallons of milk and "<<totalEggs<<" eggs.\n";
+
+
+return 0;
+
+
 }
